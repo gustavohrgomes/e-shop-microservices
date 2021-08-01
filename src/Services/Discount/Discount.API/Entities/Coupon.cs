ad90fcab669602860ffc11a98c14ef4284b3c4cf
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-08-01 15:38:41 -0300

    Create Coupon entity

---
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Entities
+{
+    public class Coupon
+    {
+        public Coupon(string productName, string description, int amount)
+        {
+            ProductName = productName;
+            Description = description;
+            Amount = amount;
+        }
+
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+    }
+}
