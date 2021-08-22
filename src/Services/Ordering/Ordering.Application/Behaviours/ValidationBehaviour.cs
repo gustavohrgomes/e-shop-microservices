dfccc9c3231e4c9e74dadd55472da32b01d4f193
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-08-22 19:34:48 -0300

    Adding EF Core Migrations for Code-First Approach in Ordering Microservices

--- src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -25,13 +25,18 @@ namespace Ordering.Application.Behaviours
             {
                 var context = new ValidationContext<TRequest>(request);
 
-                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
-                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+                var validationResults = await Task.WhenAll(
+                    _validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+                var failures = validationResults
+                    .SelectMany(r => r.Errors)
+                    .Where(f => f != null)
+                    .ToList();
 
                 if (failures.Count != 0)
                     throw new ValidationException(failures);
             }
-            
+
             return await next();
         }
     }
