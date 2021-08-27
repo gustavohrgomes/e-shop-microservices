c55f36728de64d3fd566ffd85ad5630229052989
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-08-27 07:51:42 -0300

    Subscribe BasketCheckout Queue Message Event in Ordering BasketCheckoutConsumer

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -41,6 +41,9 @@ namespace Ordering.API
             });
             services.AddMassTransitHostedService();
 
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<BasketCheckoutConsumer>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
