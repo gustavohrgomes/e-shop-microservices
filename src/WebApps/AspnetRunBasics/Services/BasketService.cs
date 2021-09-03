033d018632369293ef10dc1d1ff76054b4ba5630
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-09-03 08:10:40 -0300

    Developing AspnetBasics Shopping Web Application Microservices

---
+++ src/WebApps/AspnetRunBasics/Services/BasketService.cs
@@ -0,0 +1,44 @@
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using AspnetRunBasics.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await _client.GetAsync($"/Basket/{userName}");
+            return await response.ReadContentAs<BasketModel>();
+        }
+
+        public async Task<BasketModel> UpdateBasket(BasketModel model)
+        {
+            var response = await _client.PostAsJson<BasketModel>("/Basket", model);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("Something went wrong when calling api.");
+
+            return await response.ReadContentAs<BasketModel>();
+        }
+
+        public async Task CheckoutBasket(BasketCheckoutModel model)
+        {
+            var response = await _client.PostAsJson<BasketCheckoutModel>("/Basket/Checkout", model);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("Something went wrong when calling api.");
+        }
+
+    }
+}
---
+++ src/WebApps/AspnetRunBasics/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using AspnetRunBasics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+}
